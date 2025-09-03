fbec9039448c33d23f871b59d9e5fa5e321b7cf7
codebreaker-sketch <khanx2864@gmail.com>
2025-09-03 23:11:34 +0500

    Update app2-2.py

--- app2-2.py
+++ app2-2.py
@@ -13,8 +13,14 @@ import json
 
 # --- Firebase Init ---
 if not firebase_admin._apps:
+    # convert SecretsDict -> normal Python dict
     firebase_config = dict(st.secrets["firebase"])
-    firebase_config["private_key"] = firebase_config["private_key"].replace("\\n", "\n")
+
+    # fix private_key formatting if TOML added \n
+    if "\\n" in firebase_config["private_key"]:
+        firebase_config["private_key"] = firebase_config["private_key"].replace("\\n", "\n")
+
+    # initialize
     cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred, {
         "storageBucket": f"{firebase_config['project_id']}.appspot.com"
